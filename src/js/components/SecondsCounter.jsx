f7df56fcd1cd1f59244ca1a4ff0271d810e7cee0
JazminM08 <122406036+JazminM08@users.noreply.github.com>
2025-03-25 04:17:48 +0000

    first commit

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,60 +1,43 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-class SecondsCounter extends Component {
-    render() {
-        const { seconds } = this.props;
-        const formattedSeconds = String(seconds).padStart(6, "0").split("");
+const SecondsCounter = ({ seconds }) => {
+    const formattedSeconds = seconds.toString().padStart(6, "0");
 
-        return (
-            <div className="container d-flex justify-content-center bg-black w-50">
-                <div className="d-flex flex-row justify-content-center align-items-center row-cols-1 row-cols-md-7 g-5 p-5 w-50">
-                    <div className="col w-25 border border-1 rounded m-3 h-75">
-                        <div className="card h-10 w-auto bg-black">
-                            <div className="card1">
-                                <h5 className="card-title fs-1 text-white d-flex justify-content-center mt-3">
-                                    <i className="fa-solid fa-clock"></i>
-                                </h5>
-                            </div>
-                        </div>
-                    </div>
-
-                    {formattedSeconds.map((digit, index) => (
-                        <div key={index} className="col w-25 border border-1 rounded m-3 h-75">
-                            <div className="card h-10 w-auto bg-black">
-                                <div className={`card${index + 2}`}>
-                                    <h5 className="card-title fs-1 text-white d-flex justify-content-center mt-3">
-                                        {digit}
-                                    </h5>
-                                </div>
-                            </div>
-                        </div>
-                    ))}
+    return (
+        <div className="container d-flex justify-content-center bg-black py-4">
+            <div className="d-flex flex-row justify-content-center align-items-center gap-3">
+                
+                <div className="border border-1 rounded p-3 bg-dark text-white d-flex align-items-center justify-content-center">
+                    <i className="fa-solid fa-clock fs-1"></i>
                 </div>
+
+                {formattedSeconds.split("").map((digit, index) => (
+                    <div
+                        key={index}
+                        className="border border-1 rounded p-3 bg-dark text-white d-flex align-items-center justify-content-center"
+                        style={{ minWidth: "50px", minHeight: "80px" }}
+                    >
+                        <h5 className="fs-1">{digit}</h5>
+                    </div>
+                ))}
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { seconds: 0 };
-    }
+const App = () => {
+    const [seconds, setSeconds] = useState(0);
 
-    componentDidMount() {
-        this.interval = setInterval(() => {
-            this.setState((prevState) => ({ seconds: prevState.seconds + 1 }));
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setSeconds((prev) => prev + 1);
         }, 1000);
-    }
 
-    componentWillUnmount() {
-        clearInterval(this.interval);
-    }
+        return () => clearInterval(interval);
+    }, []);
 
-    render() {
-        return <SecondsCounter seconds={this.state.seconds} />;
-    }
-}
+    return <SecondsCounter seconds={seconds} />;
+};
 
 export default App;
 
